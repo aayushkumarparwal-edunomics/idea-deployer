d0c3a64a2b281b74d954b7b2e033771356353484
aayushkumarparwal-edunomics <aayushkumarparwal13@gmail.com>
2020-11-04 15:31:00 +0530

    frwf

--- src/Components/Challenges/IdeaCard.js
+++ src/Components/Challenges/IdeaCard.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import Slider from "@material-ui/core/Slider";
-import Typography from '@material-ui/core/Typography';
+import Collapse from "@material-ui/core/Collapse";
+import Button from "@material-ui/core/Button";
 import "../../CSS/index.css";
 const ideas = [
   {
@@ -10,7 +11,6 @@ const ideas = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Erat nam at lectus urna duis convallis. Donec et odio pellentesque diam volutpat commodo sed egestas egestas. Dui nunc mattis enim ut tellus elementum sagittis. Sodales neque sodales ut etiam sit amet nisl. Turpis in eu mi bibendum neque. A diam maecenas sed enim ut. Et egestas quis ipsum suspendisse ultrices. Orci ac auctor augue mauris augue neque gravida in. Egestas fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate. Non enim praesent elementum facilisis leo. Enim nec dui nunc mattis enim ut tellus. Nec tincidunt praesent semper feugiat nibh sed pulvinar proin. Scelerisque purus semper eget duis.",
     upVote: "12",
     downVote: "8",
-    like: "50",
     comments: [],
     effortRating: "4.7",
     impactRating: "3.5",
@@ -18,43 +18,86 @@ const ideas = [
 ];
 const useStyles = makeStyles({
   root: {
-    width: 100,
+    width: 150,
   },
 });
 
 function valuetext(value) {
-  return `${value}°C`;
+  return `${value}`;
 }
 function IdeaCard() {
   const classes = useStyles();
+  const [impact, setImpact] = React.useState(false);
+  const [effort , setEffort] = React.useState(false);
+  const handleChangeImpact = () => {
+    setImpact((prev) => !prev);
+  };
+  const handleChangeEffort = () => {
+    setEffort((prev) => !prev);
+  };
   return (
     <div>
       {ideas.map((idea) => (
         <div>
-          <h6>{idea.title}</h6>
-          <p>{idea.description}</p>
+            <h6>{idea.title}</h6>
+            <p>{idea.description}</p>
           <div>
-            <span className="cards-footer">{idea.upVote}</span>
+            <span className="cards-footer">Upvote {idea.upVote}</span>
             <span className="cards-footer">{idea.downVote}</span>
-            <span className="cards-footer">{idea.like}</span>
-            <span className={"cards-footer"+classes.root}>
+           
+            
          <div className={classes.root}>
-         <Typography id="discrete-slider" gutterBottom>
-        Impact
-      </Typography>
-      <Slider
-        defaultValue={30}
-        getAriaValueText={valuetext}
-        aria-labelledby="discrete-slider"
-        valueLabelDisplay="auto"
-        step={1}
-        marks
-        min={0.0}
-        max={5.0}
-      />
+           <Button
+          size="small"
+          id="discrete-slider"
+          checked={impact}
+          onClick={handleChangeImpact}
+        >
+          Impact {idea.impactRating}
+        </Button>
+        <div>
+          <Collapse in={impact}>
+            <Slider
+              defaultValue={0}
+              getAriaValueText={valuetext}
+              aria-labelledby="discrete-slider"
+              valueLabelDisplay="auto"
+              step={0.5}
+              marks
+              min={0.0}
+              max={5.0}
+            />
+          </Collapse>
          </div>
-            </span>
-            <span className="cards-footer">{idea.impactRating}</span>
+         </div>  
+           
+          
+           
+             <div className={classes.root}>
+           <Button
+          size="small"
+          id="discrete-slider"
+          checked={effort}
+          onClick={handleChangeEffort}
+        >
+          Effort {idea.effortRating}
+        </Button>
+        <div>
+          <Collapse in={effort}>
+            <Slider
+              defaultValue={0}
+              getAriaValueText={valuetext}
+              aria-labelledby="discrete-slider"
+              valueLabelDisplay="auto"
+              step={0.5}
+              marks
+              min={0.0}
+              max={5.0}
+            />
+          </Collapse>
+         </div>
+         </div> 
+        
           </div>
         </div>
       ))}
